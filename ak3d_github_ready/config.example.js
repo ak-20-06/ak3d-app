b866f9cabbfc3a641aa6b44118af986a6d88d9ff
ak-20-06 <asgerklaris@gmail.com>
2026-05-02 14:51:43 +0200

    Delete ak3d_github_ready directory

--- ak3d_github_ready/config.example.js
+++
@@ -1,7 +0,0 @@
-// Kopiér denne fil til config.js når Supabase skal kobles på.
-// Læg aldrig service_role key i frontend eller GitHub.
-
-window.AK3D_CONFIG = {
-  SUPABASE_URL: "https://DIT-PROJEKT.supabase.co",
-  SUPABASE_ANON_KEY: "DIN-ANON-PUBLIC-KEY"
-};
--- ak3d_github_ready/docs/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-## v0.1
-- GitHub-klar struktur
-- Supabase schema tilføjet
-- Netlify config tilføjet
