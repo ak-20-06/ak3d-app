d07836220ef03be29bc11099058599987aecf744
ak-20-06 <asgerklaris@gmail.com>
2026-06-13 12:35:38 +0200

    Use current AK3D app directly
    Removed script injection for ak3d-pages-sync.js.

--- config.js
+++ config.js
@@ -2,5 +2,3 @@ window.AK3D_CONFIG = {
   SUPABASE_URL: "https://cblmvjuhdtrtcdeyshxv.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_l-7LYGPDPT8Weyon9PNE6g_3U7uc3-J"
 };
-
-document.write('<script src="ak3d-pages-sync.js"><\/script>');
